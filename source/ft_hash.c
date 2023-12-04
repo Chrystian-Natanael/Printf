d716c2bcf18a275ecac962398967a661d6291adf
cnatanae <cnatanae@student.42sp.org.br>
2023-12-04 13:41:13 -0300

    Refactory code

--- source/ft_hash.c
+++ source/ft_hash.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 09:20:32 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/12/04 09:25:53 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/12/04 13:03:07 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,12 @@ int	ft_hash(const char *format, va_list ap)
 {
 	long long int	value;
 
+	value = (t_lolo)va_arg(ap, t_uni);
+	if (value == 0)
+		return (ft_putstr_fd("0", 1));
 	if (*format == 'x')
-	{
-		value = (t_lolo)va_arg(ap, t_uni);
-		if (value == 0)
-			return (ft_putstr_fd("0", 1));
 		return (ft_putstr_fd("0x", 1) + ft_putnbr_base(value, BASE_HEXA, ON));
-	}
 	else if (*format == 'X')
-	{
-		value = (t_lolo)va_arg(ap, t_uni);
-		if (value == 0)
-			return (ft_putstr_fd("0", 1));
 		return (ft_putstr_fd("0X", 1) + ft_putnbr_base(value, BASE_HEXA, OFF));
-	}
 	return (0);
 }
